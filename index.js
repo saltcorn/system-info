5b379fe99134195e2867c9ddf64277f01e7807ff
Tom Nielsen <tanielsen@gmail.com>
2022-07-28 17:00:43 +0100

    journald_log table

--- index.js
+++ index.js
@@ -20,6 +20,38 @@ const processes = json_list_to_external_table(async () => {
   { name: "user", type: "String" },
 ]);
 
+const journald_log = json_list_to_external_table(
+  async ({ where }) => {
+    let qs = "";
+    if (where.unit) qs += ` -u ${where.unit}`;
+    if (where.hours_ago?.lt)
+      qs += ` --since "${where.hours_ago?.lt} hours ago"`;
+
+    const sout = (revision = require("child_process")
+      .execSync(`journalctl${qs}`, { stdio: "pipe" })
+      .toString());
+    const now = new Date();
+    return sout.split("\n").map((s) => {
+      const o = JSON.parse(s);
+      const time = new Date(+o.__REALTIME_TIMESTAMP / 1000);
+      return {
+        realtime_timestamp: o.__REALTIME_TIMESTAMP,
+        unit: o.SYSLOG_IDENTIFIER,
+        message: o.MESSAGE,
+        time,
+        hours_ago: Math.abs(now - time) / 36e5,
+      };
+    });
+  },
+  [
+    { name: "realtime_timestamp", type: "String", primary_key: true },
+    { name: "unit", type: "String" },
+    { name: "message", type: "String" },
+    { name: "time", type: "Date" },
+    { name: "hours_ago", type: "Float" },
+  ]
+);
+
 const drive_usage = {
   run: async () => {
     const disks = await si.fsSize();
@@ -50,5 +82,5 @@ const mem_usage = {
 module.exports = {
   sc_plugin_api_version: 1,
   functions: { cpu_usage, drive_usage, mem_usage },
-  external_tables: { processes },
+  external_tables: { processes, journald_log },
 };
